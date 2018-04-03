24207f0d3035b7be8237084c9f685be81a76d27a
notrya <ayrtonsc6@gmail.com>
2018-04-03 16:43:38 -0500

    chat node basico

---
+++ index.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const socketIO = require("socket.io");
+
+//settings
+
+app.set('port',process.env.PORT || 3000);
+
+
+//static files
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+//start the server
+const server = app.listen(app.get('port'),() => {
+  console.log("server on port",app.get("port"));
+});
+
+const io = socketIO(server);
+
+//websockets
+
+io.on('connection',(socket)=>{
+	console.log('new connection ',socket.id);
+
+	socket.on('chat:message',(data) =>{
+		// console.log(data);
+		io.sockets.emit('chat:message',data);
+	});
+
+	socket.on('chat:typing',(data)=>{
+		socket.broadcast.emit('chat:typing',data);
+	});
+});
+
+
+
+
